14c120adff7336fcc840ec80f3beb381b0072c65
gmcgarry <>
2009-05-20 08:12:38 +0000

    Apply glibc hack for __need___va_list case.

--- libpcc/include/libpcc_stdarg.h
+++ libpcc/include/libpcc_stdarg.h
@@ -11,6 +11,12 @@
 typedef char * va_list;
 #define _VA_LIST
 #endif
+/* For broken glibc headers */
+#ifndef __GNUC_VA_LIST
+#define __gnuc_va_list void *
+#define __GNUC_VA_LIST
+#endif
+
 #endif
 
 #ifdef _LIBPCC_STDARG_H_
@@ -19,12 +25,6 @@ typedef char * va_list;
 #define va_arg(ap, type)        __builtin_va_arg((ap), type)
 #define va_end(ap)              __builtin_va_end((ap))
 #define va_copy(dest, src)      __builtin_va_copy((dest), (src))
-
-/* For broken glibc headers */
-#ifndef __GNUC_VA_LIST
-#define __gnuc_va_list void *
-#define __GNUC_VA_LIST
-#endif
 
 #endif
 
